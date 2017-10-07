dcfc149112808332f386dc59b88804ba2b299a56
Lin David, US-20 <David.Lin@bmwna.com>
2017-10-07 13:00:28 -0500

    Java Distributed miniproject2 completed

--- Java/Distributed/miniproject_2/src/main/java/edu/coursera/distributed/FileServer.java
+++ Java/Distributed/miniproject_2/src/main/java/edu/coursera/distributed/FileServer.java
@@ -2,10 +2,13 @@ package edu.coursera.distributed;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.nio.file.Files;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.File;
 
 /**
@@ -31,12 +34,9 @@ public final class FileServer {
          * ServerSocket object.
          */
         while (true) {
-
-            // TODO Delete this once you start working on your solution.
-            throw new UnsupportedOperationException();
-
             // TODO 1) Use socket.accept to get a Socket object
-
+        		Socket s = socket.accept();        	
+        	
             /*
              * TODO 2) Using Socket.getInputStream(), parse the received HTTP
              * packet. In particular, we are interested in confirming this
@@ -46,7 +46,16 @@ public final class FileServer {
              *
              *     GET /path/to/file HTTP/1.1
              */
-
+        		InputStream stream = s.getInputStream();
+        		InputStreamReader reader = new InputStreamReader(stream);
+        		BufferedReader buffered = new BufferedReader(reader);
+        		
+        		String line = buffered.readLine();
+        		assert line != null;
+        		assert line.startsWith("GET");
+        		final PCDPPath path = new PCDPPath(line.split(" ")[1]);
+        		
+        		String fileContents = fs.readFile(path);
             /*
              * TODO 3) Using the parsed path to the target file, construct an
              * HTTP reply and write it to Socket.getOutputStream(). If the file
@@ -67,6 +76,27 @@ public final class FileServer {
              *
              * Don't forget to close the output stream.
              */
+        		
+        		OutputStream out = s.getOutputStream();
+        		PrintWriter printer = new PrintWriter(out);
+        		
+        		if (fileContents == null) {
+        			printer.write("HTTP/1.0 404 Not Found\r\n");
+        			printer.write("Server: FileServer\r\n");
+        			printer.write("\r\n");
+        		}
+        		else
+        		{
+        			printer.write("HTTP/1.0 200 OK\r\n");
+        			printer.write("Server: FileServer\r\n");
+        			printer.write("\r\n");
+        			printer.write(fileContents + "\r\n");
+        		}
+        		
+        		printer.flush();
+        		printer.close();
+        		out.close();
+        		s.close();
         }
     }
 }
