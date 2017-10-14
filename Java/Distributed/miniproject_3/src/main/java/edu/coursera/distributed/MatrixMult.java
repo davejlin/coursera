aa686604e97c3d3285924b5f99e02fb6bc346c38
Lin David, US-20 <David.Lin@bmwna.com>
2017-10-14 14:57:47 -0500

    Java Distributed miniproject3 completed

--- Java/Distributed/miniproject_3/src/main/java/edu/coursera/distributed/MatrixMult.java
+++ Java/Distributed/miniproject_3/src/main/java/edu/coursera/distributed/MatrixMult.java
@@ -2,6 +2,7 @@ package edu.coursera.distributed;
 
 import edu.coursera.distributed.util.MPI;
 import edu.coursera.distributed.util.MPI.MPIException;
+import edu.coursera.distributed.util.MPI.MPI_Request;
 
 /**
  * A wrapper class for a parallel, MPI-based matrix multiply implementation.
@@ -52,7 +53,20 @@ public class MatrixMult {
      */
     public static void parallelMatrixMultiply(Matrix a, Matrix b, Matrix c,
             final MPI mpi) throws MPIException {
-        for (int i = 0; i < c.getNRows(); i++) {
+    	
+    		final int myrank = mpi.MPI_Comm_rank(mpi.MPI_COMM_WORLD);
+    		final int size = mpi.MPI_Comm_size(mpi.MPI_COMM_WORLD);
+    		
+    		final int nrows = c.getNRows();
+    		final int rowChunk = (nrows + size -1) / size;
+    		final int startRow = myrank * rowChunk;
+    		int endRow = (myrank + 1) * rowChunk;
+    		if (endRow > nrows) endRow = nrows;
+    	
+    		mpi.MPI_Bcast(a.getValues(), 0, a.getNRows() * a.getNCols(), 0, mpi.MPI_COMM_WORLD);
+    		mpi.MPI_Bcast(b.getValues(), 0, b.getNRows() * b.getNCols(), 0, mpi.MPI_COMM_WORLD);
+    		
+        for (int i = startRow; i < endRow; i++) {
             for (int j = 0; j < c.getNCols(); j++) {
                 c.set(i, j, 0.0);
 
@@ -60,6 +74,22 @@ public class MatrixMult {
                     c.incr(i, j, a.get(i, k) * b.get(k, j));
                 }
             }
+        }
+        
+        if (myrank == 0) {
+        		MPI_Request[] requests = new MPI_Request[size - 1];
+        		for (int i = 1; i < size; i++) {
+        			final int rankStartRow = i * rowChunk;
+        			int rankEndRow = (i + 1) * rowChunk;
+        			
+        			final int rowOffset = rankStartRow * c.getNCols();
+        			final int nElements = (rankEndRow - rankStartRow) * c.getNCols();
+        			
+        			requests[i - 1] = mpi.MPI_Irecv(c.getValues(), rowOffset, nElements, i, i, mpi.MPI_COMM_WORLD);
+        		}
+        		mpi.MPI_Waitall(requests);
+        } else {
+        		mpi.MPI_Send(c.getValues(), startRow * c.getNCols(), (endRow - startRow) * c.getNCols(), 0, myrank, mpi.MPI_COMM_WORLD);
         }
     }
 }
