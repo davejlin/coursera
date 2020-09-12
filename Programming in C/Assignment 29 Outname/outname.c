0ad1eeb43f0afa9d4a702bb53c4088940bc6a87e
Lin David, US-20 <David.Lin@bmwna.com>
2020-09-12 13:20:22 -0500

    complete assignment 29

--- Programming in C/Assignment 29 Outname/outname.c
+++ Programming in C/Assignment 29 Outname/outname.c
@@ -4,5 +4,9 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  // append ".counts" to inputName
+  char * newName = malloc((strlen(inputName) + 8) * sizeof(*newName));
+  strcpy(newName, inputName);
+  strcat(newName, ".counts");
+  return newName;
 }
