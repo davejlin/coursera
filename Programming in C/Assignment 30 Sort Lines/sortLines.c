3af665e340d66a3aae8fadd330a29670f4b00900
Lin David, US-20 <David.Lin@bmwna.com>
2020-09-12 16:02:06 -0500

    assignment 30

--- Programming in C/Assignment 30 Sort Lines/sortLines.c
+++ Programming in C/Assignment 30 Sort Lines/sortLines.c
@@ -16,9 +16,84 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void display(char ** data, size_t count) {
+  for (int i = 0; i < count; i++) {
+	  printf("%s\n", data[i]);
+  }
+}
+
+void freeData(char ** data, size_t count) {
+  for (int i = 0; i < count; i++) {
+	  free(data[i]);
+  }
+}
+
+struct LinesData {
+	char ** lines;
+	size_t numberOfLines;
+};
+
+struct LinesData getInput(FILE * source) {
+	size_t numberOfLines = 0;
+	size_t lineLength;
+	char * line = NULL;
+	char ** lines = NULL;
+	
+	while(getline(&line, &lineLength, source) > 0) {
+		//line[strlen(line)-1] = '\0'; // strip new line
+		lines = realloc(lines, (numberOfLines + 1) * sizeof(lines));
+		lines[numberOfLines++] = line;
+		line = NULL;
+	}
+
+	free(line);
+
+	struct LinesData linesData;
+	linesData.lines = lines;
+	linesData.numberOfLines = numberOfLines;
+	return linesData;
+}
+
+void readFromConsole() {
+	struct LinesData linesData = getInput(stdin);
+	sortData(linesData.lines, linesData.numberOfLines);
+	display(linesData.lines, linesData.numberOfLines);
+	freeData(linesData.lines, linesData.numberOfLines);
+	free(linesData.lines);
+}
+
+int readFromFile(char * fileName) {
+	FILE * file = fopen(fileName, "r");
+
+	if (file == NULL) {
+		fprintf(stderr, "%s is an invalid file", fileName);
+		return 0;
+	}
+
+	struct LinesData linesData = getInput(file);
+	sortData(linesData.lines, linesData.numberOfLines);
+	display(linesData.lines, linesData.numberOfLines);
+	freeData(linesData.lines, linesData.numberOfLines);
+	free(linesData.lines);
+
+	if(fclose(file) != 0) {
+		fprintf(stderr, "%s could not be closed", fileName);
+	}
+
+	return 1;
+}
+
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+
+  if (argc == 1) {
+	  readFromConsole();
+  } else {
+	for (int i = 1; i < argc; i++) {
+		if (readFromFile(argv[i]) == 0) {
+			return EXIT_FAILURE;
+		}
+	}
+  }
   return EXIT_SUCCESS;
 }
+
