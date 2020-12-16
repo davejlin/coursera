03a92a0101b1a8a944ba3ad653d86a19845cc05f
David Lin <dave.j.lin@gmail.com>
2020-12-15 21:50:39 -0600

    poker project 4 more

--- Programming in C/c4prj1_deck/deck.c
+++ Programming in C/c4prj1_deck/deck.c
@@ -10,6 +10,14 @@ void swapCards(card_t * c1, card_t * c2) {
 	*c2 = temp;
 }
 
+
+// This should print out the contents of a hand.
+// It should print each card (recall that
+// you wrote print_card in Course 2), and
+// a space after each card.  Do not put
+// a newline after the hand, as this
+// function gets called to print a hand
+// in the middle of a line of output.
 void print_hand(deck_t * hand){
 	card_t ** cards = hand->cards;
 	size_t nCards = hand->n_cards;
@@ -19,6 +27,14 @@ void print_hand(deck_t * hand){
 	}
 }
 
+// This function should check if a deck
+// contains a particular card or not.  If
+// the deck does contain the card, this
+// function should return 1.  Otherwise,
+// it should return 0.
+// (You will use this later to
+// build the deck of remaining cards
+// which are not in any player's hand).
 int deck_contains(deck_t * d, card_t c) {
 	card_t ** cards = d->cards;
 	size_t nCards = d->n_cards;
@@ -31,6 +47,27 @@ int deck_contains(deck_t * d, card_t c) {
 	return 0;
 }
 
+// This function takes in a deck an shuffles
+// it, randomly permuting the order of the cards.
+// There are MANY valid ways to shuffle a deck
+// of cards---we'll leave the specifics
+// of the algorithm design up to you.  However,
+// you will want to use random() to generate
+// pseudo-random numbers.  (Pseudo-random
+// numbers are quite sufficient here,
+// since they are not used for security
+// purposes). Note that you should not need to
+// use the 'srand' function.
+
+// We will note that in trying to devise
+// this algorithm, you should not
+// try to shuffle a deck of cards "normally".
+// Instead, you should take a small number
+// of cards, and think about ways
+// to shuffle them that involve using
+// random numbers to swap their order,
+// or pick positions for them, or
+// similar principles.
 void shuffle(deck_t * d) {
 	card_t ** cards = d->cards;
 	size_t nCards = d->n_cards;
@@ -41,6 +78,19 @@ void shuffle(deck_t * d) {
 	}
 }
 
+// This function should check that
+// the passed in deck contains ever
+// valid card exactly once.  If
+// the deck has any problems, this function
+// should fail an assert.  This will
+// be used to help you test your deck
+// shuffling: we will shuffle
+// a full deck, then call assert_full_deck,
+// so that you can identfiy problems with
+// the deck.  You can print
+// any error messages you want
+// if there is a problem.
+// Hint: you already wrote deck_contains.
 void assert_full_deck(deck_t * d) {
 	bool found[52] = { false };
 	for (int i = 0; i < 52; i++) {
@@ -61,6 +111,8 @@ void assert_full_deck(deck_t * d) {
 	}
 }
 
+// Add the particular card to the given deck (which will
+// involve reallocing the array of cards in that deck).
 void add_card_to(deck_t * deck, card_t c) {
 	deck->n_cards++;
 	deck->cards = realloc(deck->cards, deck->n_cards*sizeof(*deck->cards));
@@ -68,12 +120,26 @@ void add_card_to(deck_t * deck, card_t c) {
 	*deck->cards[deck->n_cards - 1] = c;
 }
 
+// Add a card whose value and suit are both 0, and return a pointer
+// to it in the deck.
+// This will add an invalid card to use as a placeholder
+// for an unknown card.
 card_t * add_empty_card(deck_t * deck) {
 	card_t * emptyCard = malloc(sizeof(*emptyCard));
 	add_card_to(deck, *emptyCard);
 	return emptyCard;
 }
 
+
+// Create a deck that is full EXCEPT for all the cards
+// that appear in excluded_cards.  For example,
+// if excluded_cards has Kh and Qs, you would create
+// a deck with 50 cards---all of them except Kh and Qs.
+// You will need to use malloc to allocate this deck.
+// (You will want this for the next function).
+// Don't forget you wrote card_t card_from_num(unsigned c)
+// in Course 2 and int deck_contains(deck_t * d, card_t c)
+// in Course 3!  They might be useful here.
 deck_t * make_deck_exclude(deck_t * excluded_cards) {
 	deck_t * deck = malloc(sizeof(*deck));
 	deck->n_cards = 0;
@@ -85,4 +151,39 @@ deck_t * make_deck_exclude(deck_t * excluded_cards) {
 		}
 	}
 	return deck;
+}
+
+// This function takes an array of hands (remember
+// that we use deck_t to represent a hand).  It then builds
+// the deck of cards that remain after those cards have
+// been removed from a full deck.  For example, if we have
+// two hands:
+// 	Kh Qs ?0 ?1 ?2 ?3 ?4
+// 	As Ac ?0 ?1 ?2 ?3 ?4
+// then this function should build a deck with 48
+// cards (all but As Ac Kh Qs).  You can just build
+// one deck with all the cards from all the hands
+// (remember you just wrote add_card_to),
+// and then pass it to make_deck_exclude.
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) {
+	// make a deck with all the known cards in hands
+	deck_t * cardsToExclude = malloc(sizeof(*cardsToExclude));
+	cardsToExclude->n_cards = 0;
+	cardsToExclude->cards = NULL;
+
+	for (int i = 0; i < n_hands; i++) {
+		deck_t * hand = hands[i];
+		for (int j = 0; j < hand->n_cards; j++) {
+			card_t * card = hand->cards[j];
+			if (card->value > 0) {
+				size_t currentSize = cardsToExclude->n_cards;
+				cardsToExclude->cards = realloc(cardsToExclude->cards, (currentSize+1)*sizeof(*cardsToExclude->cards));
+				cardsToExclude->cards[currentSize] = malloc(sizeof(*card));
+				cardsToExclude->cards[currentSize] = card;
+				cardsToExclude->n_cards++;
+			}
+		}
+	}
+
+	return make_deck_exclude(cardsToExclude);
 }
--- Programming in C/c4prj1_deck/test.c
+++ Programming in C/c4prj1_deck/test.c
@@ -29,4 +29,38 @@ int main(int argc, char ** argv) {
     deck_t * deck2 = make_deck_exclude(deck1);
     print_hand(deck2);
     printf("\n");
+
+    deck_t * deck3 = malloc(sizeof(*deck1));
+    deck3->n_cards = 0;
+    deck3->cards = NULL;
+
+    card.suit = SPADES;
+    card.value = 2;
+
+    add_card_to(deck3, card);
+
+    card.suit = DIAMONDS;
+    card.value = VALUE_QUEEN;
+
+    add_card_to(deck3, card);
+
+    card.suit = CLUBS;
+    card.value = 4;
+
+    add_card_to(deck3, card);
+    add_empty_card(deck3);
+    add_empty_card(deck3);
+
+    deck_t ** hands = malloc(2*sizeof(*hands));
+    hands[0] = malloc(sizeof(deck1));
+    hands[0]->cards = deck1->cards;
+    hands[0]->n_cards = deck1->n_cards;
+
+    hands[1] = malloc(sizeof(deck3));
+    hands[1]->cards = deck3->cards;
+    hands[1]->n_cards = deck3->n_cards;
+
+    deck_t * remainingDeck = build_remaining_deck(hands, 2);
+    print_hand(remainingDeck);
+    printf("\n");
 }
