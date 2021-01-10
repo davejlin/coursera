925c7a925cee139c482d8bddd67fa0c94b110453
David Lin <dave.j.lin@gmail.com>
2021-01-10 15:31:47 -0600

    hide functions inside sqrt block

--- Scala/Functional Programming Principles In Scala/sqrt/sqrt/src/main/scala/sqrt.scala
+++ Scala/Functional Programming Principles In Scala/sqrt/sqrt/src/main/scala/sqrt.scala
@@ -1,13 +1,15 @@
 class sqrt {
-    def sqrtIter(guess: Double, x: Double): Double =
-        if (isGoodEnough(guess, x)) guess
-        else sqrtIter(improve(guess, x), x)
+    def sqrt(x: Double) = {
+        def sqrtIter(guess: Double, x: Double): Double =
+            if (isGoodEnough(guess, x)) guess
+            else sqrtIter(improve(guess, x), x)
 
-    def isGoodEnough(guess: Double, x: Double): Boolean =
-        (guess * guess - x).abs / x < 0.0001
+        def isGoodEnough(guess: Double, x: Double): Boolean =
+            (guess * guess - x).abs / x < 0.0001
 
-    def improve(guess: Double, x: Double): Double =
-        (guess + x / guess) / 2
+        def improve(guess: Double, x: Double): Double =
+            (guess + x / guess) / 2
 
-    def sqrt(x: Double) = sqrtIter(1.0, x)
+        sqrtIter(1.0, x)
+    }
 }
