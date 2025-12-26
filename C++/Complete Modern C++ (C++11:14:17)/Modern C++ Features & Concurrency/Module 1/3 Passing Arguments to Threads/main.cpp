1ef5a0afa31ede962111d842a4682c56a6cae909
David Lin <dave.j.lin@gmail.com>
2025-12-26 09:18:22 -0700

    add Passing Arguments to Threads

---
+++ C++/Complete Modern C++ (C++11:14:17)/Modern C++ Features & Concurrency/Module 1/3 Passing Arguments to Threads/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+#include <thread>
+
+using namespace std;
+list<int> g_Data;
+const int SIZE = 50000000;
+
+class String {
+public:
+    String() {
+        cout << "String created\n";
+    }
+    String(const String& other) {
+        cout << "String copied\n";
+    }
+    String & operator=(const String& other) {
+        cout << "String assigned\n";
+        return *this;
+    }
+    ~String() {
+        cout << "String destroyed\n";
+    }
+    friend ostream& operator<<(ostream& os, const String& str) {
+        os << "[String Object]";
+        return os;
+    }
+};
+
+void Download(String &file) {
+    cout << "[Download] Started download of file:" << file << "\n";
+
+    for (int i = 0; i < SIZE; ++i) {
+        g_Data.push_back(i);
+    }
+
+    cout << "[Download] Finished download\n";
+}
+
+int main() {
+    String file;
+    cout << "[Main] Starting download task\n";
+    thread downloadThread(Download, ref(file));
+    downloadThread.detach();
+    cout << "[Main] Download task completed\n";
+
+    if (downloadThread.joinable()) {
+        downloadThread.join();
+    }
+
+    //system("pause"); <-- Uncomment this line if you want to pause the console on Windows
+    cin.get(); // Wait for user input before closing to effectively "pause" the console for Mac/Linux
+    return 0;
+}
