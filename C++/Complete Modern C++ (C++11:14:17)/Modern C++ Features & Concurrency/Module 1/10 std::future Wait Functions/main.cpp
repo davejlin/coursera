83f383e31d28944eb738c1b014fe38f342d1dc6b
David Lin <dave.j.lin@gmail.com>
2025-12-27 10:03:47 -0700

    add future Wait Functions

---
+++ C++/Complete Modern C++ (C++11:14:17)/Modern C++ Features & Concurrency/Module 1/10 std::future Wait Functions/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+#include <future>
+
+using namespace std;
+
+int Operation(const int count) {
+    using namespace chrono_literals;
+    int sum = 0;
+    for (int i = 0; i < count; ++i) {
+        cout << "." << flush;
+        sum += i;
+        this_thread::sleep_for(500ms); // std::chrono literal for 500 milliseconds
+    }
+    return sum;
+}
+
+int main() {
+    using namespace chrono_literals;
+    // if launch::deferred is used, the operation will synchronously run only when get() or wait() is called on the future
+    future<int> result = async(launch::async, Operation, 10);
+    this_thread::sleep_for(1s);
+    cout << "[Main] Thread continues execution ... \n";
+    if (result.valid()) {
+        auto timepoint = chrono::system_clock::now();
+        timepoint += 1s;
+        auto status = result.wait_until(timepoint);
+       // auto status = result.wait_for(1s);
+       switch (status) {
+           case future_status::ready:
+               cout << "\nOperation completed within 1 second.\n";
+               break;
+           case future_status::timeout:
+               cout << "\nOperation not completed within 1 second.\n";
+               break;
+           case future_status::deferred:
+               cout << "\nOperation is deferred.\n";
+               break;
+       }
+       auto sum = result.get();
+       cout << "\nOperation result: " << sum << "\n";
+    }
+    return 0;
+}
