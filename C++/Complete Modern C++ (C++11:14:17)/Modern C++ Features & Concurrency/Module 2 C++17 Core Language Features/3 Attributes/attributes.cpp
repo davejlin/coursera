01cf75ca76ce04706a9d00b2ba11cc6cf2373be9
David Lin <dave.j.lin@gmail.com>
2025-12-29 09:48:46 -0700

    add Attributes

---
+++ C++/Complete Modern C++ (C++11:14:17)/Modern C++ Features & Concurrency/Module 2 C++17 Core Language Features/3 Attributes/attributes.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+[[deprecated("Use the new version instead")]]
+int * CreateIntArray(const size_t size) {
+    return new int[size];
+}
+
+template<typename T>
+[[nodiscard]] // Warn if the return value is discarded
+T * CreateArray(size_t size) {
+    return new T[size];
+}
+
+class [[deprecated]] Number {};
+Number GetNumber(int value) {
+    return Number{};
+}
+
+class [[deprecated("OldClass is deprecated, use NewClass instead")]] OldClass {
+public:
+    void Display() {
+        std::cout << "This is the old class.\n";
+    }
+};
+
+namespace [[deprecated("This namespace is deprecated")]] DeprecatedNamespace {
+    [[deprecated("OldFunction is deprecated, use NewFunction instead")]]
+    void OldFunction() {
+        std::cout << "This function is deprecated.\n";
+    }
+}
+
+int main() {
+    int* myArray = CreateIntArray(10);
+    // Use the array...
+    delete[] myArray;
+    
+    CreateArray<double>(20); // Warning if the return value is discarded
+    
+    GetNumber(5); // Warning about deprecated class
+
+    OldClass oldObj;
+    oldObj.Display();
+
+    DeprecatedNamespace::OldFunction();
+    
+    return 0;
+}
