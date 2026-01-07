8942d35129562c3b45337d2d09a9740f7c4a1326
David Lin <dave.j.lin@gmail.com>
2026-01-07 10:09:53 -0700

    add Filesystem - directory_entry

---
+++ C++/Complete Modern C++ (C++11:14:17)/Modern C++ Features & Concurrency/Module 5 Standard Library Components/11 Filesystem - directory_entry/filesystem.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = filesystem ;
+
+void UsingPath() {
+	fs::path selectedPath{R"(E:\Data\Material\C++\Assignments.docx)"} ;
+	cout << selectedPath << endl;
+	cout << selectedPath.string() << endl;
+
+	//selectedPath.remove_filename() ;
+	//selectedPath /= "newfile" ;
+	
+	if (selectedPath.has_root_name()) {
+		cout << "root name\t = " << selectedPath.root_name().string() << endl;
+	}
+	if (selectedPath.has_root_path()) {
+		cout << "root path\t = " << selectedPath.root_path().string() << endl;
+	}
+	if (selectedPath.has_root_directory()) {
+		cout << "root directory\t = " << selectedPath.root_directory().string() << endl;
+	}
+	if (selectedPath.has_parent_path()) {
+		cout << "parent path\t = " << selectedPath.parent_path().string() << endl;
+	}
+	if (selectedPath.has_relative_path()) {
+		cout << "relative path\t = " << selectedPath.relative_path().string() << endl;
+	}
+	if (selectedPath.has_filename()) {
+		cout << "filename\t = " << selectedPath.filename().string() << endl;
+	}
+	if (selectedPath.has_stem()) {
+		cout << "stem part \t = " << selectedPath.stem().string() << endl;
+	}
+	if (selectedPath.has_extension()) {
+		cout << "extension\t = " << selectedPath.extension().string() << endl;
+	}
+}
+
+void TraversingDirectory(string_view file) {
+	fs::path currentPath{file} ;
+	vector<fs::directory_entry> dir_entries{} ;
+	//fs::directory_iterator begin{currentPath} ;
+	//fs::directory_iterator end{} ;
+	//while(begin != end) {
+	//	auto de = *begin++ ;
+	//	cout << de.path().filename() << endl; 
+	//}
+	for(const auto & dir_entry : fs::directory_iterator{currentPath}) {
+		dir_entries.push_back(dir_entry) ;
+	}
+    
+	partition(dir_entries.begin(), dir_entries.end(), [](const fs::directory_entry & de) {
+		return de.is_directory() ;
+	});
+
+	for(const auto &dir_entry : dir_entries) {
+		switch(const auto &p = dir_entry.path() ;/*dir_entry.status().type()*/ fs::status(p).type()) {
+		case fs::file_type::directory:
+			cout << "[DIR]\t" << p.string() << endl; 
+			break ;
+		case fs::file_type::regular:
+			cout << '\t' << p.string() << '\t' << dir_entry.file_size() << endl; 
+			break ;
+		}
+	}
+}
+
+int main() {
+	UsingPath();
+    TraversingDirectory("/Users/davilin/Documents/Projects/coursera/");
+}
